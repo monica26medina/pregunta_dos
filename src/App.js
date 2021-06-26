7239ab589025bb54f3695cdc6aa5d83d3c49f9a1
Monica Medina <medinaquisberth@gmail.com>
2021-06-26 10:58:41 -0400

    commit principal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Dashboard from "./components/dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <div className='menu'>
+          <Link to='/'>Home</Link>
+          <br />
+          <Link to='/about'>About</Link>
+          <br />
+          <Link to='/dashboard'>Dashboard</Link>
+        </div>
+        <hr />
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
